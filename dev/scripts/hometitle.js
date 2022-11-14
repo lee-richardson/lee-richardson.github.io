2a4ed5e468708a691f306983c3367eac3206e057
Lee Richardson <lee.richardson4@gmail.com>
2022-11-14 18:42:30 +0100

    changing js file

--- dev/scripts/hometitle.js
+++ dev/scripts/hometitle.js
@@ -1,15 +1,13 @@
-
-
 "use strict";
 
 /* replace homepage title */
 function homeReplace(id, content) {
   var container = document.getElementById(id);
-  container.innerHTML = content;
+  container.innerHTML = content; 
 };
 
 var containerId = "change";
-var strings = ["a Product designer", "🇬🇧🇨🇦", "designing in Figma", "a former journalist", "a fan of dogs"];
+var strings = ["a Product designer", "lære dansk 🧠", "designing in Figma", "a UX writer", "a fan of dogs"];
 var count = 0;
 var timerId = setInterval(cycle, 1200);
 function cycle() {
