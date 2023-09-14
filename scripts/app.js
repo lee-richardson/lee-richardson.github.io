e7aadbe1d832c15ecf6d582a48119b262664fa61
Lee Richardson <lee.richardson4@gmail.com>
2023-09-14 12:06:43 +0200

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+
+/* replace homepage title */
+  // function homeReplace(id, content) {
+  //   var container = document.getElementById(id);
+  //   container.innerHTML = content;
+  // };
+
+  // var containerId = "change";
+  // var strings = ["a product designer.", "a front-end developer.", "a former journalist.", "designing in Figma.", "a fan of dogs.", "an Android user.", "an expat 🇬🇧🇨🇦 in CPH."];
+  // var count = 0;
+  // var timerId = setInterval(cycle, 1700);
+  // function cycle() {
+  //   if(count < strings.length) {
+  //     homeReplace(containerId,strings[count]);
+  //     count++;
+  //   } else {
+  //     count=0;
+  //   }
+  // };
+// 
---
+++ scripts/hometitle.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* replace homepage title */
+function homeReplace(id, content) {
+  var container = document.getElementById(id);
+  container.innerHTML = content; 
+};
+
+var containerId = "change";
+var strings = ["a Product designer", "lære dansk 🧠", "designing in Figma", "a UX writer", "a fan of dogs"];
+var count = 0;
+var timerId = setInterval(cycle, 1200);
+function cycle() {
+  if (count < strings.length) {
+    homeReplace(containerId, strings[count]);
+    count++;
+  } else {
+    count = 0;
+  }
+};
+
+var backgroundDefault = "#fff";
+var background1 = '#fed876';
+
+
